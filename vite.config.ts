c397f2105fa65ef51bb2d5f6b6be2cd44247afde
Luca <lucameopou@gmail.com>
2026-03-15 12:22:38 +0100

    chore(config): enable TypeScript path resolution in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
+	resolve: {
+		tsconfigPaths: true,
+	},
 	plugins: [
 		nitro({
 			vercel: {
